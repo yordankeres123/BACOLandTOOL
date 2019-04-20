178fa6d152559f3f7eb92601246a8b508594f4b1
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-20 14:31:44 +0700

    Add files via upload
    DOSA GOBLOKKKKKKK!!!

---
+++ BACOL.sh
@@ -0,0 +1,45 @@
+clear
+figlet -f BIGBOS STARLA
+
+sleep 1
+echo "\033[33;1m<====================================================================>"
+echo "\033[31;1m<>Tool By Bigbos Starla<>"
+echo "\033[31;1mTeam :CYB3R POP1CE +62"
+echo "\033[31;1mWA :tebak sendiri"
+echo "\033[31;1mFB : Bigbos Starla"
+echo "\034[35;1mYT :Bigbos Starla subcribe yah cuk"
+echo "\033[31;1mFriedns : MR.GHOST"
+echo "\033[31;1mTanggal Di Buat : 00 00 0000"
+echo "\033[33;1m<===================================================================>"
+echo
+echo
+echo $purple "_____________________________" | lolcat
+echo $red ">>"   $cyan "1.14toolstermux"
+echo $red ">>"   $cyan "2.pornotools"
+echo $purple "____________________________" | lolcat
+echo
+echo
+read -p "#rootstarla~#$" pilih
+if [ $pilih = 1 ]
+then
+clear
+sleep 1
+pkg install python
+pkg install git
+git clone https://github.com/yordankeres123/14toolstermux
+cd 14toolstermux
+sh 15ToolsTermux.sh
+fi
+
+if [ $pilih = 2 ]
+then
+clear
+figlet BACOL | lolcat
+sleep 1
+pkg install python
+pkg install git
+git clone https://github.com/yordankeres123/pornotools
+cd pornotools
+ls
+sh P*rno.sh
+fi
